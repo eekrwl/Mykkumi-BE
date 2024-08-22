c647d30fa0ef1674b6719c6e2adc7bf15bdd1bd4
안채완 <59831262+eekrwl@users.noreply.github.com>
2024-08-22 13:16:08 +0900

    Update MykkumiServerApplicationTests.java

--- src/test/java/com/swmarastro/mykkumiserver/MykkumiServerApplicationTests.java
+++ src/test/java/com/swmarastro/mykkumiserver/MykkumiServerApplicationTests.java
@@ -2,6 +2,7 @@ package com.swmarastro.mykkumiserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("dev")
 @SpringBootTest
